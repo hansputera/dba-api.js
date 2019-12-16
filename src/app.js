a8428340c3d9622f51d47bfc08da7f304050074c
hansputera <47862061+hansputera@users.noreply.github.com>
2019-12-16 17:42:17 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,20 +5,20 @@ const config = require("../config.json")
 module.exports = class DiscordBotsAlchemist {
   
   constructor(id, token){
-    if (!id) throw new Error("[dba-api.js] ID Must be provided ( your id bot )")
-    if (!token) throw new Error("[dba-api.js] Token must be provided") // Checking something
+    if (!id) throw new Error("[dba-api.js] Please enter ID")
+    if (!token) throw new Error("[dba-api.js] Please Enter Token") // Checking something
     
     
-    if (typeof id !== "string") throw Error("[dba-api.js] ID must be included by string")
-    if (typeof token !== "string") throw Error("[dba-api.js] Token must be included by string")
+    if (typeof id !== "string") throw Error("[dba-api.js] ID must be wrapped with a string")
+    if (typeof token !== "string") throw Error("[dba-api.js] Token must be wrapped with a string")
     
     
-    if (isNaN(id)) throw new Error("[dba-api.js] ID not allowed for alphabet.")
-    if (id.length <= 17) throw new Error("[dba-api.js] ID length must be 18")
-    if (id.length > 18) throw new Error("[dba-api.js] ID length must be 18")
+    if (isNaN(id)) throw new Error("[dba-api.js] ID is not allowed for letter characters")
+    if (id.length <= 17) throw new Error("[dba-api.js] Your bot id characters cannot be fewer than 22 characters")
+    if (id.length > 18) throw new Error("[dba-api.js] Your bot id characters cannot be fewer than 22 characters ID length must be 18")
     
-    if (token.length <= 21) throw new Error("[dba-api.js] Your length token must be 22")
-    if (token.length > 22) throw new Error("[dba-api.js] Your length token must be 22")
+    if (token.length <= 21) throw new Error("[dba-api.js] Token characters cannot be fewer than 22 characters")
+    if (token.length > 22) throw new Error("[dba-api.js] Token characters cannot be fewer than 22 characters")
     
     this.version = require("../package.json").version;
     this.baseURL = "https://dba-dev.glitch.me"
@@ -32,7 +32,7 @@ module.exports = class DiscordBotsAlchemist {
         .then(data => {
       
       
-      if (!data[id]) throw Error("[dba-api.js] Your ID Bot isn't registered on database")
+      if (!data[id]) throw Error("[dba-api.js] Your bot ID is not in the database")
       if (data[id].token !== token) throw Error("[dba-api.js] Your token is wrong !")
       
       console.log(`You logged as : ${data[id].bot} with token : (hidden)`)
@@ -40,10 +40,10 @@ module.exports = class DiscordBotsAlchemist {
     })
 
     this.getBot = async function(id) {
-      if (!id) throw Error("[dba-api.js] Empety ID")
+      if (!id) throw Error("[dba-api.js] Please Enter ID")
       if (isNaN(id)) throw Error("[dba-api.js] Invalid ID")
       if (id.length <= 17 || id.length > 18) throw Error("[dba-api.js] Invalid Id")
-      if (typeof id !== 'string') throw Error("[dba-api.js] That's ID isn't string")
+      if (typeof id !== 'string') throw Error("[dba-api.js] The ID isn't a string")
       
      /* 
       fetch(this.baseAPIURL + `/bots/${id}`).then(a => a.json()).then(p => {
@@ -81,15 +81,15 @@ module.exports = class DiscordBotsAlchemist {
 this.fetchUser = async function(id) {
   
  // if (!id) throw Error("[dba-api.js] Empety ID")
-  if (!id) throw Error("[dba-api.js] Empety ID");
+  if (!id) throw Error("[dba-api.js] Please enter ID");
   if (isNaN(id)) throw Error("[dba-api.js] Invalid Id");
-  if (typeof id !== "string") throw Error("[dba-api.js] That's ID isn't string");
+  if (typeof id !== "string") throw Error("[dba-api.js] The ID is not a string");
   if (id.length <= 17) throw Error("[dba-api.js] Invalid Id");
   if (id.length > 18) throw Error("[dba-api.js] Invalid Id");
   
   fetch(this.baseAPIURL + `/fetchUser?id=${id}`).then(data => data.json()).then(p =>{
     
-    if (p.error === "404") throw Error("[dba-api.js] Requested to server was timeout")
+    if (p.error === "404") throw Error("[dba-api.js] Request timeout")
     
     const body = {
       username: p.username,
